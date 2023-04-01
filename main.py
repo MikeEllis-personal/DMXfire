91e304ccd3c9c62dd81f96b3cb4a514ebe40abc4
Mike Ellis <github@mikeellis.org.uk>
2023-04-01 10:23:22 +0100

    Adding the DMX to firelight committed Only the vaguest template at the moment - as much to prove that the GitHub integration is working correctly as anything else at the moment.

---
+++ main.py
@@ -0,0 +1,13 @@
+import dmx
+import fire
+
+# Initialise the DMX receiver
+dmx_start_channel = 130
+
+# Initialise the fire effect
+
+while True:
+    # Read latest DMX values and adjust fire flicker parameters appropriately
+
+    # Update the firelight
+    
