d8fe0a31a0b62c6de65f7dd6c44f6b6cff6ce71f
Mike Ellis <github@mikeellis.org.uk>
2023-04-01 11:40:45 +0100

    Added PyLance type hints for RP2 assembler

--- fire.py
+++ fire.py
@@ -1,12 +1,8 @@
 import array
-from machine import Pin
-import rp2
+from machine import Pin           # type: ignore
+import rp2                        # type: ignore
 from time import sleep
 from random import seed, randint
-
-import array, time
-from machine import Pin
-import rp2
 
 # Build the PIO driver for the WS2812 led array
 @rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
@@ -14,14 +10,14 @@ def ws2812():
     T1 = 2
     T2 = 5
     T3 = 3
-    wrap_target()
-    label("bitloop")
-    out(x, 1)               .side(0)    [T3 - 1]
-    jmp(not_x, "do_zero")   .side(1)    [T1 - 1]
-    jmp("bitloop")          .side(1)    [T2 - 1]
-    label("do_zero")
-    nop()                   .side(0)    [T2 - 1]
-    wrap()
+    wrap_target()                                          # type: ignore
+    label("bitloop")                                       # type: ignore
+    out(x, 1)               .side(0)    [T3 - 1]           # type: ignore
+    jmp(not_x, "do_zero")   .side(1)    [T1 - 1]           # type: ignore
+    jmp("bitloop")          .side(1)    [T2 - 1]           # type: ignore
+    label("do_zero")                                       # type: ignore
+    nop()                   .side(0)    [T2 - 1]           # type: ignore
+    wrap()                                                 # type: ignore
 
 fade_r         = 0.8
 fade_g         = 0.5
