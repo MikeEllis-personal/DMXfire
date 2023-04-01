9b48b3bca380a41844769fe157de729d8ba9fbe6
Mike Ellis <github@mikeellis.org.uk>
2023-04-01 11:09:57 +0100

    Configure the DMX input pin For the demo hardware, GPIO28 is used (Pico pin 34)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import fire
 
 # Initialise the DMX receiver
 dmx_start_channel = 130
+dmx_in  = DMX_RX(pin = 28) # DMX data should be presented to GPIO28 (Pico pin 34)
+dmx_in.start()
 
 # Initialise the fire effect
 
@@ -10,4 +12,4 @@ while True:
     # Read latest DMX values and adjust fire flicker parameters appropriately
 
     # Update the firelight
-    
+    pass
