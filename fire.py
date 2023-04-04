c0f60edd66186e237ff41dc2fc4fdd45c523e2e2
Mike Ellis <github@mikeellis.org.uk>
2023-04-04 20:20:02 +0100

    Refactored the firelight code Code is now structured as a parameterised class which is simply called to update the entire panel. The PIO ASM is also now in a separate source file to make maintenance easier.

--- fire.py
+++ fire.py
@@ -1,37 +1,91 @@
 import array
-from machine import Pin           # type: ignore
-import rp2                        # type: ignore
 from time import sleep
 from random import seed, randint
+from machine import Pin
+import rp2                            # type: ignore
+from neopixel import ws2812
 
-# Build the PIO driver for the WS2812 led array
-@rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
-def ws2812():
-    T1 = 2
-    T2 = 5
-    T3 = 3
-    wrap_target()                                          # type: ignore
-    label("bitloop")                                       # type: ignore
-    out(x, 1)               .side(0)    [T3 - 1]           # type: ignore
-    jmp(not_x, "do_zero")   .side(1)    [T1 - 1]           # type: ignore
-    jmp("bitloop")          .side(1)    [T2 - 1]           # type: ignore
-    label("do_zero")                                       # type: ignore
-    nop()                   .side(0)    [T2 - 1]           # type: ignore
-    wrap()                                                 # type: ignore
 
-fade_r         = 0.8
-fade_g         = 0.5
-fade_b         = 0.3
+class led_panel:
+    def __init__(self, pin, leds, statemachine=0):
+        # Create the StateMachine with the ws2812 program, outputting on Pin(22).
+        self._sm = rp2.StateMachine(0, ws2812, freq=8_000_000, sideset_base=Pin(pin))
+        self._sm.active(1)
+        self._strip = array.array("I", [0 for _ in range(leds)]) # GRB colour order
+
+    def __del__(self):
+        # TODO - tidy up the state machine
+        pass
+
+    def __repr__(self):
+        # TODO - encode the class state - including which PIO
+        pass
+
+    def __str__(self):
+        # Convert the state to a string
+        pass
+
+    @staticmethod
+    def firelight(strip, brightness, fade, speed, blocks, dim):
+        fade_r         = 0.8  # How quickly the redness fades
+        fade_g         = 0.5  # How quickly the greenness fades
+        fade_b         = 0.3  # How quickly the blueness fades
+        warmth         = 60   # Maximum percentage component of green
+        coolth         = 5    # Maximum percentage component of blue
+
+        # First fade everything out slightly
+        for led in range(len(strip)):
+            # Read the current colour
+            red   = (strip[led] >>  8) & 0xff
+            green = (strip[led] >> 16) & 0xff
+            blue  = (strip[led] >>  0) & 0xff
+            
+            # Fade it a little bit
+            red   = int(red   * fade_r * fade / 255)
+            green = int(green * fade_g * fade / 255)
+            blue  = int(blue  * fade_b * fade / 255)
+            strip[led] = (green << 16) + (red << 8) + blue
+
+        # Next sometimes brighten some blocks up again
+        if (randint(0,255) <= speed):
+            leds_per_block = int(len(strip)/blocks)
+            for block in range(blocks - dim):
+                start_led = randint(0,blocks-1) * leds_per_block
+                end_led   = start_led + leds_per_block
+
+                # Set an intensity for each LED in the block
+                for led in range(start_led, end_led):
+                    red   = randint(warmth,    255)
+                    green = randint(warmth//2, (red   * warmth) // 100)
+                    blue  = randint(0,         (green * coolth) // 100)
+
+                    red   = int(red   * brightness / 255)
+                    green = int(green * brightness / 255)
+                    blue  = int(blue  * brightness / 255)
+                    strip[led] = (green << 16) + (red << 8) + blue
+
+    @staticmethod
+    def fill(strip, r, g, b):
+        value = (b & 0xff) + (r & 0xff) << 8 + (g & 0xff) << 16
+        for led in range(len(strip)):
+            strip[led] = value
+
+    def update(self, r=0, g=0, b=0, brightness=0, fade=255, speed=0, blocks=8, dim=3):
+        if r>0 or g>0 or b>0:
+            # If any of RGB are non-zero, create a simple colour wash as specified
+            led_panel.fill(self._strip, r, g, b)
+        else:
+            # Create a fire-like effect
+            led_panel.firelight(self._strip, brightness, fade, speed, blocks, dim)
+
+        self._sm.put(self._strip, 8)
+
+"""
+
 leds           = 256
-blocks         = 8
-dimblocks      = 3
-leds_per_block = int(leds/blocks)
 glow           = 40
-warmth         = 60   # Maximum percentage component of green
-coolth         = 5    # Maximum percentage component of blue
-wait           = 0.01 # Pause between cycles
 cycletime      = 6    # Number of loops between brightenings
-
+wait           = 0.01 # Pause between cycles
 
 def fade(strip):
     # Fade all LEDs a bit
@@ -105,3 +159,4 @@ sm.active(1)
 
 selftest(sm)
 fire_effect(sm)
+"""
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from dmx import DMX_RX
+from fire import led_panel
 from time import sleep_ms
 
-def dmx_fire():
+def dmx_test():
     # Initialise the DMX receiver
     dmx_start_channel = 128
     dmx_in  = DMX_RX(pin=28) # DMX data should be presented to GPIO28 (Pico pin 34)
@@ -18,3 +19,14 @@ def dmx_fire():
                 print(f"{dmx_in.channels[dmx_start_channel+n]:3}  ", end="")
             print(f" IRQ#:{irq}")
             last_irq = irq
+
+def fire_test():
+    # Initialise the firelight effect
+    firelight = led_panel(pin=27, leds=256)
+
+    while True:
+        firelight.update(brightness = 255, 
+                         fade       = 64,
+                         speed      = 255,
+                         blocks     = 16,
+                         dim        = 6)
